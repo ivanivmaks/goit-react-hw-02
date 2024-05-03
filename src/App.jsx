df8d791fc888ac48f970fc6e2067b315e77a9144
ivaniv_maks <ivanivmaks0@gmail.com>
2024-05-03 12:37:14 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,9 @@ import Notification from "./components/Notification/Notification";
 export default function App() {
   const [feedback, setFeedback] = useState(() => {
    const savedFeedback = window.localStorage.getItem('feedback');
-    if (savedFeedback !== null) {
-      return JSON.parse(savedFeedback);
-    }
-    return { good: 0, neutral: 0, bad: 0 };
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
   })
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
